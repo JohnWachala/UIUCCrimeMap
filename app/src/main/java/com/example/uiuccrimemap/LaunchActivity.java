4fcc9a65a9e98a43c758c44caf868c6340a75919
John Wachala <jwacha4@illinois.edu>
2019-11-30 18:46:44 -0600

    created LaunchActivity

---
+++ app/src/main/java/com/example/uiuccrimemap/LaunchActivity.java
@@ -0,0 +1,15 @@
+package com.example.uiuccrimemap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class LaunchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}

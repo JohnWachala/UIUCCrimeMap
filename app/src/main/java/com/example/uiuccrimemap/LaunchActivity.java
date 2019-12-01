865b5047ac5e4d7f7f7fe6bbcdf52127eea50550
John Wachala <jwacha4@illinois.edu>
2019-11-30 19:45:52 -0600

    Launch Page button switches to map view

--- app/src/main/java/com/example/uiuccrimemap/LaunchActivity.java
+++ app/src/main/java/com/example/uiuccrimemap/LaunchActivity.java
@@ -1,6 +1,9 @@
 package com.example.uiuccrimemap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,12 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        Button launchStartButton = (Button) findViewById(R.id.startButton);
+        launchStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(LaunchActivity.this, MapsActivity.class));
+            }
+        });
     }
 }
